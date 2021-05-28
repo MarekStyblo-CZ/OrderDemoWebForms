969f1c1d6e9a862b7704ff2cc0c4dd7728a66907
Marek Stýblo <marek.styblo@aftg.cz>
2021-05-28 10:58:48 +0200

    Product db model

---
+++ OrderDemoWebForms/Model/DbSet/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OrderDemoWebForms.Model.DbSet
+{
+    public class Product
+    {
+        //PK
+        [Key, Column(Order = 0), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //Relations
+        //public List<OrderItem> OrderItems { get; set; }
+
+        //Attr
+
+        public int Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
