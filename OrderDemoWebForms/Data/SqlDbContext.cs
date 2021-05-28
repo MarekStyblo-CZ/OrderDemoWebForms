9ca331f6de723fa70904f855324ec105d92fdea5
Marek Stýblo <marek.styblo@aftg.cz>
2021-05-28 11:00:46 +0200

    setup Entity framework

---
+++ OrderDemoWebForms/Data/SqlDbContext.cs
@@ -0,0 +1,15 @@
+﻿using OrderDemoWebForms.Model.DbSet;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OrderDemoWebForms.Data
+{
+    public class SqlDbContext : DbContext, IDisposable
+    {
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ OrderDemoWebForms/ReadMe.txt
@@ -0,0 +1,6 @@
+﻿--older EF install
+In package Manager console I had to:
+0] In connection string is only 1! backslash ..(localdb)\MSSQLLocalDB...
+1] Enable-Migrations
+2] Add-migration Init
+3] Update-database
