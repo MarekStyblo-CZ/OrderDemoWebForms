26fa66e80472736af35b758818f1598c45b4d6c1
Marek Stýblo <marek.styblo@aftg.cz>
2021-05-29 09:54:11 +0200

    migration for order and orderItem

---
+++ OrderDemoWebForms/Migrations/202105290746131_OrderWithOrderItems.cs
@@ -0,0 +1,49 @@
+﻿namespace OrderDemoWebForms.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class OrderWithOrderItems : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Price = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductId)
+                .Index(t => t.OrderId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CustomerName = c.String(nullable: false),
+                        CustomerAddress = c.String(nullable: false),
+                        Created = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderItems", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.OrderItems", "OrderId", "dbo.Orders");
+            DropIndex("dbo.OrderItems", new[] { "ProductId" });
+            DropIndex("dbo.OrderItems", new[] { "OrderId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderItems");
+        }
+    }
+}
--- OrderDemoWebForms/Migrations/Configuration.cs
+++ OrderDemoWebForms/Migrations/Configuration.cs
@@ -13,18 +13,23 @@
             AutomaticMigrationsEnabled = false;
         }
 
-        //Is run automatically - no need to generate migration (resp. its not seen in migrations..)
+        ////  This method will be called after migrating to the latest version.
+        // Is run automatically - no need to generate migration (resp. its not seen in migrations..)
         protected override void Seed(OrderDemoWebForms.Data.SqlDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
-
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method to avoid creating duplicate seed data.
             context.Products.AddOrUpdate(new Product { Id = 1, Code = 111, Name = "Pračka", Price = 7600.5f });
             context.Products.AddOrUpdate(new Product { Id = 2, Code = 112, Name = "Myčka", Price = 9800.6f });
-            context.SaveChanges();
 
+            context.Orders.AddOrUpdate(new Order { Id = 1, CustomerName = "Aleš Vopěnka", CustomerAddress = "Uliční 22, Těškov", Created = new DateTime(2020, 05, 20) });
+            context.Orders.AddOrUpdate(new Order { Id = 2, CustomerName = "Jan Novák", CustomerAddress = "Testovací 11, Zadov", Created = new DateTime(2020, 05, 21) });
+
+            context.OrderItems.AddOrUpdate(new OrderItem { Id = 1, OrderId = 1, ProductId = 1, Price = 7600.5f, Quantity = 1 });
+            context.OrderItems.AddOrUpdate(new OrderItem { Id = 2, OrderId = 2, ProductId = 2, Price = 9800.6f, Quantity = 1 });
+
+            context.SaveChanges();
         }
+
+
     }
 }
