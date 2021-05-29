2559b1003c674803e02624f0617c484531e481fe
Marek Stýblo <marek.styblo@aftg.cz>
2021-05-29 09:53:36 +0200

    seed data

--- OrderDemoWebForms/Data/SqlDbContext.cs
+++ OrderDemoWebForms/Data/SqlDbContext.cs
@@ -1,15 +1,32 @@
 ﻿using OrderDemoWebForms.Model.DbSets;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
 
 namespace OrderDemoWebForms.Data
 {
     public class SqlDbContext : DbContext, IDisposable
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Prevent deleting Product when relation to OrderItem
+            //1:N
+            modelBuilder.Entity<Product>()
+                .HasMany<OrderItem>(p => p.OrderItems)
+                .WithRequired(oi => oi.Product)
+                .HasForeignKey(oi => oi.ProductId)
+                .WillCascadeOnDelete(false);
+
+            //1:N
+            modelBuilder.Entity<Order>()
+                .HasMany<OrderItem>(o => o.OderItems)
+                .WithRequired(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId)
+                .WillCascadeOnDelete(true);
+        }
     }
 }
